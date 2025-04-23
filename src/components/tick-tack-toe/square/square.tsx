0baed8f9bec495470f9f7b9137d32f33802a88eb
Tristan Wiese <tristan.wiese@klearium.com>
2025-04-23 09:45:44 +0200

    fix eslint errors

--- src/components/tick-tack-toe/square/square.tsx
+++ src/components/tick-tack-toe/square/square.tsx
@@ -1,33 +1,32 @@
-import React, { MouseEventHandler, useState } from 'react'
+import React, { useState } from "react";
 
-import './square.css'
+import "./square.css";
 
-interface SquareType{
-  text:string
-  handleClick: CallableFunction
-  id: number
-  playing:boolean
+interface SquareType {
+  text: string;
+  handleClick: CallableFunction;
+  id: number;
+  playing: boolean;
 }
 
-const Square:React.FC<SquareType> = ({text, handleClick, id, playing}) => {
-
-  function click(){
-    if (hasPlayed || !playing){
-      return
+const Square: React.FC<SquareType> = ({ text, handleClick, id, playing }) => {
+  function click() {
+    if (hasPlayed || !playing) {
+      return;
     }
     handleClick(id);
     sethasPlayed(true);
     setstate(text);
   }
 
-  const [state, setstate] = useState<string>('')
-  const [hasPlayed, sethasPlayed] = useState<boolean>(false)
+  const [state, setstate] = useState<string>("");
+  const [hasPlayed, sethasPlayed] = useState<boolean>(false);
 
   return (
-    <div className='square-container prevent-select' onClick={click}>
+    <div className="square-container prevent-select" onClick={click}>
       {state}
     </div>
-  )
-}
+  );
+};
 
-export default Square
+export default Square;
--- src/pages/portfolio/portfolio.tsx
+++ src/pages/portfolio/portfolio.tsx
@@ -1,79 +1,73 @@
-import './portfolio.css'
+import "./portfolio.css";
 
-import React from 'react'
+import React from "react";
 
-import ProjectCard from './components/projectCard';
-import ProjectsHeader from './components/header/projectsHeaders';
+import ProjectCard from "./components/projectCard";
+import ProjectsHeader from "./components/header/projectsHeaders";
 
-import { 
-    GET_ALL,
-    DROP_N_GO,
-    DROP_N_GO_README,
-    FLUTTER_CHATBOT,
-    FLUTTER_CHATBOT_README,
-    FLUTTER_FIDGET,
-    FLUTTER_FIDGET_README,
-    FLUTTER_MISC_APPS,
-    FLUTTER_MISC_APPS_README,
-    MAIN_URL,
-    PHP_MISC_PROJECTS,
-    PHP_MISC_PROJECTS_README,
-    POCKET_PUPS,
-    POCKET_PUPS_README,
-    KEYLOGGER_TIMER,
-    KEYLOGGER_TIMER_README,
-} from './consts/projectURLS';
+import {
+  DROP_N_GO,
+  DROP_N_GO_README,
+  FLUTTER_CHATBOT,
+  FLUTTER_CHATBOT_README,
+  FLUTTER_FIDGET,
+  FLUTTER_FIDGET_README,
+  FLUTTER_MISC_APPS,
+  FLUTTER_MISC_APPS_README,
+  PHP_MISC_PROJECTS,
+  PHP_MISC_PROJECTS_README,
+  POCKET_PUPS,
+  POCKET_PUPS_README,
+  KEYLOGGER_TIMER,
+  KEYLOGGER_TIMER_README,
+} from "./consts/projectURLS";
 
-function Portfolio(){
-
-    let html = (
-    
-        <div className='portfolio-main'>
-            <ProjectsHeader/>
-            <div className='portfolio-grid'>
-                <ProjectCard 
-                    title='Drop N GO' 
-                    url={DROP_N_GO} 
-                    descriptionUrl={DROP_N_GO_README}
-                />
-                <ProjectCard 
-                    title='Pocket Pups' 
-                    url={POCKET_PUPS} 
-                    descriptionUrl={POCKET_PUPS_README}
-                />
-                <ProjectCard 
-                    title='Flutter Chat Bot' 
-                    url={FLUTTER_CHATBOT} 
-                    descriptionUrl={FLUTTER_CHATBOT_README}
-                />
-                <ProjectCard 
-                    title='React Keylogger Timer' 
-                    url={KEYLOGGER_TIMER} 
-                    descriptionUrl={KEYLOGGER_TIMER_README}
-                    deploy='https://faythsix.co.za/tristan/keylogger/'
-                />
-                <ProjectCard 
-                    title='Flutter Fidget' 
-                    url={FLUTTER_FIDGET} 
-                    descriptionUrl={FLUTTER_FIDGET_README}
-                />
-                <ProjectCard 
-                    title='Flutter Misc Apps' 
-                    url={FLUTTER_MISC_APPS} 
-                    descriptionUrl={FLUTTER_MISC_APPS_README}
-                />
-                <ProjectCard 
-                    title='PHP Misc Projects' 
-                    url={PHP_MISC_PROJECTS} 
-                    descriptionUrl={PHP_MISC_PROJECTS_README}
-                    deploy='https://faythsix.co.za/tristan/php/product-store/'
-                />
-            </div>
-
-        </div>
-
-    );
-    return html;
+function Portfolio() {
+  let html = (
+    <div className="portfolio-main">
+      <ProjectsHeader />
+      <div className="portfolio-grid">
+        <ProjectCard
+          title="Drop N GO"
+          url={DROP_N_GO}
+          descriptionUrl={DROP_N_GO_README}
+        />
+        <ProjectCard
+          title="Pocket Pups"
+          url={POCKET_PUPS}
+          descriptionUrl={POCKET_PUPS_README}
+        />
+        <ProjectCard
+          title="Flutter Chat Bot"
+          url={FLUTTER_CHATBOT}
+          descriptionUrl={FLUTTER_CHATBOT_README}
+        />
+        <ProjectCard
+          title="React Keylogger Timer"
+          url={KEYLOGGER_TIMER}
+          descriptionUrl={KEYLOGGER_TIMER_README}
+          deploy="https://faythsix.co.za/tristan/keylogger/"
+        />
+        <ProjectCard
+          title="Flutter Fidget"
+          url={FLUTTER_FIDGET}
+          descriptionUrl={FLUTTER_FIDGET_README}
+        />
+        <ProjectCard
+          title="Flutter Misc Apps"
+          url={FLUTTER_MISC_APPS}
+          descriptionUrl={FLUTTER_MISC_APPS_README}
+        />
+        <ProjectCard
+          title="PHP Misc Projects"
+          url={PHP_MISC_PROJECTS}
+          descriptionUrl={PHP_MISC_PROJECTS_README}
+          deploy="https://faythsix.co.za/tristan/php/product-store/"
+        />
+      </div>
+    </div>
+  );
+  return html;
 }
 
-export default Portfolio
+export default Portfolio;
